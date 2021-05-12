ae00703bfcb89efbbae897136e25885d43ba0293
AbassWeb <84043057+AbassWeb@users.noreply.github.com>
2021-05-12 23:09:39 +0100

    Add files via upload

--- ragingstreets.py
+++ ragingstreets.py
@@ -4,10 +4,13 @@ items = []
 
 def print_pause(message_to_print):
     print(message_to_print)
-    time.sleep(2)
+    time.sleep(0)
 
-print_pause("Welcome to the Streets of Rage."
-"You will have to fight your way through this level to win!")
+enemies = ["Zamza", "Vehelits"]
+
+def start():
+    print_pause("Welcome to the Streets of Rage."
+            "You will have to fight your way through this level to win!")
 
 print_pause("Please choose your character:")
 character = input("1. Axel\n"
@@ -15,16 +18,16 @@ character = input("1. Axel\n"
                     "3. Adam\n")
 
 print_pause(f"You have chosen {character}\n"
-            "a great warrior indeed")
+                "a great warrior indeed")
 
 while True:
     while True:
 
         print_pause(f"{character} You have landed on the Streets of Rage\n"
-                "you must choose which area to travel to first")
+                "you must choose which area to travel to")
         response = input("East street, West street, South street\n").lower()
 
-        if "East" in response:
+        if "east" in response:
             print_pause("You take the train to East street, which "
                         "contains a small shopping precint.")
             if "Broadsword" in items:
@@ -40,15 +43,39 @@ while True:
                             "'This should serve you well, but to prove effective,"
                             "it requires a power cell found in another street'\n")
                 items.append("Broadsword")
+            print_pause("You now decide to travel to another part of town")
 
-        elif "West" in response:
-                print_pause("You take the tram to West street, which "
-                            "contains a small shopping precint.")
-                print_pause("You now find yourself in the magic district.")
+        elif "west" in response:
+            print_pause("You take the tram to West street, where "
+                        "you are drawn to a glowing haze of light .")
+            if "Power cell" in items:
+                print_pause("The necromancer suggests you head to the South, "
+                            "where some dangerous creatured have been "
+                            "terrorising the locals.")
+            else:
+                if "Broadsword" in items:
+                    print_pause("You meet a necromancer, who gifts you with a "
+                                "Power cell.  This enhances your swords power ."
+                                "This also gives you great visibility at night.")
+                    items.append("Power cell")
+                else:
+                    print_pause("The necromancer greets you sternly "
+                                "'I am the necromancer, the enhancer of"
+                                "weapons.  Only come back to me once your"
+                                "have your weapon.  Stop wasting my time'")
+            print_pause("You now decide to travel to another part of town")
 
-        elif "South" in response:
-                print_pause("You take the train to South street, which "
-                            "is shrouded in an eerie darkness.")
-                print_pause("After a few moments, you here a frigtening roar.")
+        elif "south" in response:
+            print_pause("You take the train to South street, which "
+                        "is shrouded in an eerie darkness.")
+            print_pause("After a few moments, you here a frigtening roar.")
+            print_pause("Here the dangerous creatures lurk")
+            if "Power cell" in items:
+                print_pause("Your power cell provides some much needed light ")
+                print_pause(enemy = random.choice(enemies))
+                print_pause("Zamza, a powerful mutant suddenly appears, but luckily "
+                            "you see him in good time, ready to anticipate his attack.")
 
-    print_pause("Where would you like to go next?")
+        print_pause("Where would you like to go next?")
+
+    start()
